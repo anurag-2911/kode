79e4f5cd2b93c4f6e378c30538c75d87a9db44ad
Anurag Kumar <anurag.hcu@gmail.com>
2024-05-04 00:42:42 +0530

    array progs

--- programming/progs/prog01_test.go
+++ programming/progs/prog01_test.go
@@ -2,6 +2,8 @@ package progs
 
 import (
 	"fmt"
+	"log"
+	"reflect"
 	"sort"
 	"testing"
 )
@@ -49,34 +51,95 @@ func TestLongestConsecutiveSequence(t *testing.T) {
 }
 
 func (arops ArrayOps) longestConsecutiveSequence(nums []int) int {
-	if len(nums)==0{
+	if len(nums) == 0 {
 		return 0
 	}
 	sort.Slice(nums, func(i, j int) bool {
-		return nums[i]<nums[j]
+		return nums[i] < nums[j]
 	})
-	current,longest:=1,1
+	current, longest := 1, 1
 
-	for i:=1;i<len(nums);i++{
-		if nums[i]==nums[i-1]+1{
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1]+1 {
 			current++
-		}else{
-			if current>longest{
-				longest=current
+		} else {
+			if current > longest {
+				longest = current
 			}
-			current=1
+			current = 1
 		}
 	}
 
-	if current>longest{
-		longest=current
+	if current > longest {
+		longest = current
 	}
 
 	return longest
 }
 
 /*
-Two Sum Problem:
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Find the "Kth" Largest Element in an Array:
+Given an integer array and a number k, return the kth largest element in the array.
+*/
+func TestKthLargestNum(t *testing.T) {
+	tests := []struct {
+		Name     string
+		nums     []int
+		k        int
+		expected int
+	}{
+		{"test1", []int{101, 2, 56, 78, 9, 11}, 3, 56},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			result := arrayOps.kthlargest(test.nums, test.k)
+			if result != test.expected {
+				log.Printf("expected %v, found %v", test.expected, result)
+			}
+		})
+	}
+}
+func (aops *ArrayOps) kthlargest(nums []int, k int) int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] > nums[j]
+	})
+	return nums[k-1]
+}
+
+/*
+Move Zeroes:
+Given an array nums, write a function to move all 0's to the end of it
+while maintaining the relative order of the non-zero elements.
 */
 
+func (aops *ArrayOps) movezeros(nums []int) []int {
+	j := 0 // index to put non zero element
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			if i != j {
+				nums[j], nums[i] = nums[i], nums[j]
+			}
+			j++
+		}
+	}
+	return nums
+}
+func TestMoveZeros(t *testing.T) {
+	tests := []struct {
+		Name     string
+		nums     []int
+		expected []int
+	}{
+		{"test1", []int{0, 12, 0, 34, 2, 3, 4, 0, 0, 67}, []int{12, 34, 2, 3, 4, 67, 0, 0, 0, 0}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			result := arrayOps.movezeros(test.nums)
+			if !reflect.DeepEqual(result, test.expected) {
+				log.Printf("expected %v, got %v", test.nums, result)
+			}
+		})
+	}
+}
